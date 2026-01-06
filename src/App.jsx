ec6af3a47d8e2adc26a3214d844d760b87ae20b4
ajitdevv <ajitdev014@gmail.com>
2026-01-06 22:11:30 +0530

    your message

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-
+import  mehndiImages  from "./data/mehndiphotos";
 function App() {
   return (
-   <h1></h1>
-  )
+    <div className="grid grid-cols-4 gap-2 p-4">
+      {mehndiImages.map((image) => (
+        <img
+          className="size-cover w-full h-full rounded-lg"
+          key={image.id}
+          src={image.url}
+          alt={`Mehndi ${image.id}`}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
