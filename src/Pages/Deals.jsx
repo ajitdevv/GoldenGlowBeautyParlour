6042539c1238bb681b2d762de99fda7fb8774924
ajitdevv <ajitdev014@gmail.com>
2026-03-26 14:54:43 +0530

    Allmost Complate

--- src/Pages/Deals.jsx
+++ src/Pages/Deals.jsx
@@ -64,6 +64,7 @@ const Deals = () => {
           <AddButton children={"Add Deals"} onClick={() => AddDeal()} />
         </div>
       </div>
+      <div></div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
         {loader && (
           <div className="w-full min-w-245 max-w-7xl mx-auto px-4">
